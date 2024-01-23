c6b90d46db35638d737ec450be337d75fba2707c
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-01-23 22:41:20 +0000

    Retrieving stored values
    - Tidying up code

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// CALENDAR
-
-// READ day.js DOCUMENTATION and apply it in the code
-
-// Structure of the web
-//* Display the current day at the top of the calender when a user opens the planner.
-
-// Create blocks with times of the day where an user can write events
-// - Select <p id="currentDay" class="lead"></p> and apply day.js code 
-// so that it is up to date with calendar
 function getDayName(date) {
     return date.toLocaleDateString(undefined, { weekday: 'long' });    
 }
@@ -15,9 +5,11 @@ function getDayName(date) {
 function getMonthName(date) {
     return date.toLocaleDateString(undefined, { month: 'long' });    
 }
+
 function getTime(date) {
-    return new Date(new Date().setHours(10)).toLocaleString(undefined, { hour: 'numeric'});
+    return new Date(new Date().setHours(11)).toLocaleString(undefined, { hour: 'numeric' } );
 }
+
 function nthNumber(number) {
     if (number > 3 && number < 21) return "th";
     switch (number % 10) {
@@ -37,40 +29,35 @@ var strDate = d.getDate();
 var formattedDate = `${getDayName(d)}, ${getMonthName(d)} ${strDate}${nthNumber(strDate)}`;
 $("#currentDay").append(formattedDate);
 
-// - Select <div class="container"> and create the blocks in the div
 var blockEl = $(".container");
 const workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (let i = 0; i < workingHours.length; i++) {
   let currentTime = getTime(d);
   let inputEl;
-
-  if (currentTime == workingHours[i]) {
-    inputEl = $(`<input type="text" class="col col-10 description${[i]} present"/>`);
-  }
-  else if (currentTime < workingHours[i]) {
-    inputEl = $(`<input type="text" class="col col-10 description${[i]} future"/>`);
-  }
-  else if (currentTime > workingHours[i]) {
-    inputEl = $(`<input type="text" class="col col-10 description${[i]} past"/>`);
-  }
   const rowEl = $(`<div class="row">`);
   const hourEl = $(`<div class="col col-1 hour">${workingHours[i]}</div>`);
-  const btnEl = $(`<button id="${i}" class="col col-1 saveBtn">SAVE</button>`);
+  let btnEl = $(`<button id="${i}" class="col col-1 saveBtn">SAVE</button>`);
+  let storedEvent = localStorage.getItem(`event${i}`) || "";
+
+  if (currentTime == workingHours[i]) {
+    inputEl = $(`<input type="text" class="col col-10 description${[i]} present" value=${storedEvent}>`);
+  }
+  else if (currentTime < workingHours[i]) {
+    inputEl = $(`<input type="text" class="col col-10 description${[i]} future" value=${storedEvent}>`);
+  }
+  else if (currentTime > workingHours[i]) {
+    inputEl = $(`<input type="text" disabled class="col col-10 description${[i]} past" value=${storedEvent}>`);
+    btnEl = $(`<button id="${i}" disabled class="col col-1 saveBtn">SAVE</button>`)
+  }
   
   blockEl.append(rowEl);
   rowEl.append(hourEl);
   rowEl.append(inputEl);
   rowEl.append(btnEl);
-  
 } 
 
 $(".saveBtn").on("click", function() {
-
   const input = $(`.description${this.id}`).val();
-  console.log(input);
-  localStorage.setItem("event", JSON.stringify(input));
+  localStorage.setItem(`event${this.id}`, input);
 });
-
-// Save event in local storage when save button is clicked in that timeblock.
-// It must stay saved when refresh
